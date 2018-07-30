643cad0b21140daf83ce374f8c65231d0d332091
Caio Lente <ctlente@gmail.com>
2018-07-30 15:05:20 -0300

    Esqueleto pacote

--- R/nupputils.R
+++ R/nupputils.R
@@ -1,29 +1,37 @@
-library(tidyverse)
+
+#' Filtrar assuntos
+#' @export
 assuntos_filtrar <- function() {
   c(
     "Advocacia administrativa",
-    "Concussão",
-    "Excesso de exação",
-    "Extravio,? sonegação",
-    "Inserção de dados falsos",
-    "corrupção(?! de menor)",
-    "Lavagem ou Ocultação de Bens",
-    "não autorizada",
+    "Concuss\u00e3o",
+    "Excesso de exa\u00e7\u00e3o",
+    "Extravio,? sonega\u00e7\u00e3o",
+    "Inser\u00e7\u00e3o de dados falsos",
+    "corrup\u00e7\u00e3o(?! de menor)",
+    "Lavagem ou Oculta\u00e7\u00e3o de Bens",
+    "n\u00e3o autorizada",
     "Peculato",
-    "Contra a Administração em Geral",
-    "Tráfico de influência"
+    "Contra a Administra\u00e7\u00e3o em Geral",
+    "Tr\u00e1fico de influ\u00eancia"
   ) %>%
     tibble::enframe() %>%
     dplyr::mutate(value = value %>%
                     stringr::str_to_upper() %>%
                     abjutils::rm_accent())
 }
+
+#' Limpar texto
+#' @param txt Texto
 txt_clean <- function(txt) {
   txt %>%
     stringr::str_to_upper() %>%
     abjutils::rm_accent() %>%
     stringr::str_squish()
 }
+
+#' Extrair processos de arquivo
+#' @param file Arquivo
 all_lawsuits_file <- function(file) {
   message(file)
   pag <- readr::read_rds(file)
@@ -48,10 +56,16 @@ all_lawsuits_file <- function(file) {
                     stringr::str_pad(20, "left", "0")) %>%
     dplyr::select(cnj, id, tipo = value, page, txt)
 }
+
+#' Pesquisar assunto
+#' @param x Assunto
+#' @param txt_data Dados do texto
 pesquisar_assunto <- function(x, txt_data = all_texts_pags_clean) {
   message(x)
-  filter(txt_data, str_detect(txt, x))
+  dplyr::filter(txt_data, stringr::str_detect(txt, x))
 }
+
+#' Regex com padrao CNJ
 padrao_cnj <- function(){
   glue::glue(
     "[0-9]{{3,7}}-?",
@@ -62,15 +76,21 @@ padrao_cnj <- function(){
     "[0-9]{{4}}"
   ) %>% as.character()
 }
+
+#' Ler todos os textos
+#' @param all_text_files Todos os arquivos
 read_all_txt <- function(all_text_files) {
   all_text_files %>%
-    set_names(.) %>%
+    purrr::set_names(.) %>%
     {pb <<- progress::progress_bar$new(total = length(.));.} %>%
     purrr::map_dfr(~{
       pb$tick()
       tibble::tibble(txt = readr::read_file(.x))
     }, .id = "file")
 }
+
+#' Quebrar paginas
+#' @param all_texts Todos os textos
 break_pags <- function(all_texts) {
   all_texts %>%
     dplyr::mutate(txt = stringr::str_split(txt, pattern = '\\f')) %>%
@@ -79,15 +99,22 @@ break_pags <- function(all_texts) {
     dplyr::mutate(pag = 1:n()) %>%
     dplyr::ungroup()
 }
+
+#' Ler textos em paralelo
+#' @param all_text_files Todos os arquivos
 read_all_txt_parallel <- function(all_text_files) {
   all_text_files %>%
-    set_names(.) %>%
+    purrr::set_names(.) %>%
     {pb <<- progress::progress_bar$new(total = length(.));.} %>%
     purrr::map_dfr(~{
       pb$tick()
       tibble::tibble(txt = readr::read_file(.x))
     }, .id = "file")
 }
+
+#' Construir paginas
+#' @param files Arquivos
+#' @param cores Nucleos de processamento
 build_pages <- function(files, cores = 10) {
   files %>%
     purrr::set_names(.) %>%
@@ -103,6 +130,10 @@ build_pages <- function(files, cores = 10) {
     dplyr::ungroup() %>%
     dplyr::select(id, page, txt = output)
 }
+
+#' Escrever paginas
+#' @param arqs_list Lista de arquivos
+#' @param chunks Numero de blocos
 write_pages <- function(arqs_list, chunks = 100) {
   purrr::iwalk(arqs_list, ~{
     message(.y)
@@ -126,10 +157,13 @@ write_pages <- function(arqs_list, chunks = 100) {
     })
   })
 }
+
+#' Coletar processos
+#' @param arqs_list Lista de arquivo
 get_lawsuits <- function(arqs_list) {
   # arqs_list <- fs::dir_ls("diarios", type = "directory") %>%
   #   purrr::map(fs::dir_ls, regexp = "pages") %>%
-  #   purrr::map(fs::path_abs)
+  #   purr::map(fs::path_abs)
   purrr::imap_dfr(arqs_list, ~{
     message(.y)
     res <- purrr::map_dfr(purrr::set_names(.x, .x),
---
+++ R/utils.R
@@ -0,0 +1,14 @@
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @importFrom magrittr %>%
+NULL
+
+globalVariables(c(
+  "txt", "value", "res", "cnj", "id", "page", "n", ".", "output",
+  "all_texts_pags_clean"))
